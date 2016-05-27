be562b897b5d507ebf74e01f5ba0b3f09ae876a9
Marcus Ottosson <marcus@abstractfactory.io>
2016-05-27 10:45:10 +0000

    Add PyPI support

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+version = "0.1"
+
+
+classifiers = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.5",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: Utilities"
+]
+
+
+setup(
+    name="Qt",
+    version=version,
+    description="Python 2 & 3 compatibility wrapper around all Qt bindings - PySide, PySide2, PyQt4 and PyQt5.",
+    author="Marcus Ottosson",
+    author_email="konstruktion@gmail.com",
+    url="https://github.com/mottosso/Qt",
+    license="MIT",
+    zip_safe=False,
+    py_modules=["iscompatible"],
+    classifiers=classifiers,
+)
