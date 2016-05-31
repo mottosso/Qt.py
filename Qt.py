adb12efbdd380e55aae373b512763baf9a55ae36
Marcus Ottosson <marcus@abstractfactory.io>
2016-05-31 09:50:36 +0000

    Fix broken installer

--- Qt.py
+++ Qt.py
@@ -21,7 +21,7 @@ Usage:
 import os
 import sys
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 
 
 def _pyqt5():
@@ -104,6 +104,12 @@ def _init():
     preferred = os.getenv("QT_PREFERRED_BINDING")
 
     if preferred:
+        
+        # Debug mode, used in installer
+        if preferred == "None":
+            sys.modules[__name__].__wrapper_version__ = __version__
+            return
+
         available = {
             "PySide2": _pyside2,
             "PySide": _pyside,
