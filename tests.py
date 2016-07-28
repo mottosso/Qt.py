73920a9924f64a952eb4c4024faa240243c3b4b6
Fredrik Averpil <fredrik.averpil@gmail.com>
2016-07-29 01:37:36 +0200

    Revert "Remove QMenuBar and QStatusBar from UI"
    This reverts commit c61b33d1f97982d12f7b2a49fc18f21b0fdd4d1b.

--- tests.py
+++ tests.py
@@ -205,6 +205,18 @@ def test_load_ui_into_self_pyside():
     </item>
    </layout>
   </widget>
+  <widget class="QMenuBar" name="menubar">
+   <property name="geometry">
+    <rect>
+     <x>0</x>
+     <y>0</y>
+     <width>125</width>
+     <height>22</height>
+    </rect>
+   </property>
+  </widget>
+  <widget class="QStatusBar" name="statusbar"/>
+ </widget>
  <resources/>
  <connections/>
 </ui>"""
@@ -251,6 +263,18 @@ def test_load_ui_into_self_pyqt4():
     </item>
    </layout>
   </widget>
+  <widget class="QMenuBar" name="menubar">
+   <property name="geometry">
+    <rect>
+     <x>0</x>
+     <y>0</y>
+     <width>125</width>
+     <height>22</height>
+    </rect>
+   </property>
+  </widget>
+  <widget class="QStatusBar" name="statusbar"/>
+ </widget>
  <resources/>
  <connections/>
 </ui>"""
@@ -297,6 +321,18 @@ def test_load_ui_into_custom_pyside():
     </item>
    </layout>
   </widget>
+  <widget class="QMenuBar" name="menubar">
+   <property name="geometry">
+    <rect>
+     <x>0</x>
+     <y>0</y>
+     <width>125</width>
+     <height>22</height>
+    </rect>
+   </property>
+  </widget>
+  <widget class="QStatusBar" name="statusbar"/>
+ </widget>
  <resources/>
  <connections/>
 </ui>"""
@@ -343,6 +379,18 @@ def test_load_ui_into_custom_pyqt4():
     </item>
    </layout>
   </widget>
+  <widget class="QMenuBar" name="menubar">
+   <property name="geometry">
+    <rect>
+     <x>0</x>
+     <y>0</y>
+     <width>125</width>
+     <height>22</height>
+    </rect>
+   </property>
+  </widget>
+  <widget class="QStatusBar" name="statusbar"/>
+ </widget>
  <resources/>
  <connections/>
 </ui>"""
