7ebadcc33f9c17678c2454c227ba75c00ada262b
Fredrik Averpil <fredrik.averpil@gmail.com>
2016-09-18 16:07:52 +0200

    Add expected output

--- tests.py
+++ tests.py
@@ -316,6 +316,36 @@ def test_pyside2uic():
 
     import pyside2uic
 
+    expected_output = """\
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'diggi.ui'
+#
+# Created: Sun Sep 18 13:57:29 2016
+#      by: pyside2-uic  running on PySide2 2.0.0~alpha0
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide2 import QtCore, QtGui, QtWidgets
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(235, 149)
+        self.gridLayout = QtWidgets.QGridLayout(Form)
+        self.gridLayout.setObjectName("gridLayout")
+        self.lineEdit = QtWidgets.QLineEdit(Form)
+        self.lineEdit.setObjectName("lineEdit")
+        self.gridLayout.addWidget(self.lineEdit, 0, 0, 1, 1)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QtWidgets.QApplication.translate("Form", "Form", None, -1))
+
+"""
+
     with open(self.ui_qwidget, 'r') as ui_file:
         # from cStringIO import StringIO
         # Read compiled UI into string
@@ -328,8 +358,9 @@ def test_pyside2uic():
         py_filename = 'output.py'
         with open(py_filename, 'w') as py_file:
             pyside2uic.compileUi(ui_file, py_file)
-            import output
+            # import output
             # ui_form = output.Ui_Form()  # Create object
+            assert expected_output == py_file.read()
         os.remove(py_filename)
 
 
