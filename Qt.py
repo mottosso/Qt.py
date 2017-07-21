35f2b0cdd56d51d51fa14a958c36eeb4a27cc75d
Mike <mikeh@blur.com>
2017-07-20 18:39:42 -0700

    Prevent error reported in #218

--- Qt.py
+++ Qt.py
@@ -1399,7 +1399,8 @@ def _install():
             setattr(our_submodule, member, their_member)
 
     # Backwards compatibility
-    Qt.QtCompat.load_ui = Qt.QtCompat.loadUi
+    if hasattr(Qt.QtCompat, 'loadUi'):
+        Qt.QtCompat.load_ui = Qt.QtCompat.loadUi
 
 
 _install()
