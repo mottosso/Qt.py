6f086ba94e62e05f0588382a4835ef3a42b48d67
Fredrik Averpil <fredrik.averpil@gmail.com>
2017-08-15 23:26:40 +0200

    Perform tests using python 2.7, 3.4, 3.5

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
-cp -r /Qt.py /workdir
+# Start Xvfb
 Xvfb :99 -screen 0 1024x768x16 2>/dev/null &
-while ! ps aux | grep -q '[0]:00 Xvfb :99 -screen 0 1024x768x16';
-    do echo "Waiting for Xvfb..."; sleep 1; done
+while ! ps aux | grep -q '[0]:00 Xvfb :99 -screen 0 1024x768x16'; do
+    echo "Waiting for Xvfb..."
+    sleep 1
+done
 
-echo "#\n# Running tests in Python ${PYTHON}"
+printf "#\n# Running tests in Python ${PYTHON}\n"
 export NOSETESTS_BINARY=nosetests${PYTHON}
 
-echo "#\n# Testing implementation.."
+printf "#\n# Testing implementation..\n"
     python${PYTHON} -u run_tests.py
-echo "#\n# Testing caveats..\n#"
+printf "#\n# Testing caveats..\n"
     python${PYTHON} build_caveats.py
     nosetests${PYTHON} \
         --verbose \
         --with-doctest \
         --with-process-isolation \
         test_caveats.py
-echo "#\n# Testing membership..\n#"
+printf "#\n# Testing membership..\n"
     python${PYTHON} build_membership.py
     nosetests${PYTHON} \
         --verbose \
         test_membership.py
-echo "#\n# Testing examples..\n#"
+printf "#\n# Testing examples..\n"
     nosetests${PYTHON} \
     --verbose \
     --with-process-isolation \
     --with-doctest \
     --exe \
         examples/*/*.py
-echo Done
+printf Done
