204b968424eefb1667e3090ff860eb6eaae5481c
Fredrik Averpil <fredrik@averpil.com>
2017-08-18 10:58:21 +0200

    Add test_docker.sh

---
+++ test_docker.sh
@@ -0,0 +1,3 @@
+docker run --rm -v $(pwd):/Qt.py -e PYTHON=2.7 fredrikaverpil/qt.py:2017
+docker run --rm -v $(pwd):/Qt.py -e PYTHON=3.4 fredrikaverpil/qt.py:2017
+docker run --rm -v $(pwd):/Qt.py -e PYTHON=3.5 fredrikaverpil/qt.py:2017
