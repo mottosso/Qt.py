556fe78bc68cbc5e8ba75ad3ab51888756a2802d
Fredrik Averpil <fredrik@averpil.com>
2017-08-31 18:44:44 +0200

    Recurse further

--- membership.py
+++ membership.py
@@ -4,6 +4,7 @@ import pkgutil
 BINDINGS = (__import__('PySide'), __import__('PySide2'), __import__('PyQt4'),
             __import__('PyQt5'))
 
+MODULES = []
 
 if __name__ == '__main__':
     for package in BINDINGS:
@@ -11,4 +12,20 @@ if __name__ == '__main__':
                 path=package.__path__,
                 prefix=package.__name__ + '.',
                 onerror=lambda x: None):
-            print(modname)
+            MODULES.append(modname)
+            base = modname[:modname.rfind('.')]
+            mod = modname[modname.rfind('.')+1:]
+            try:
+                exec('from ' + base + ' import ' + mod)
+                exec('raw_members = dir(' + mod + ')')
+
+                members = []
+                for member in raw_members:
+                    if not member.startswith('_'):
+                        print(modname + '.' + member)
+                        MODULES.append(modname + '.' + member)
+            except:
+                pass
+
+    for modname in MODULES:
+        print(modname)
