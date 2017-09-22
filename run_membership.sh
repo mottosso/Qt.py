9617aa1a05ce5112fc187502ae26e9d5ced4108a
Fredrik Averpil <fredrik@averpil.com>
2017-09-22 19:20:40 +0200

    Use 2018 docker image to run membership.py

--- run_membership.sh
+++ run_membership.sh
@@ -7,14 +7,14 @@ set -e
 array=( PySide PySide2 PyQt4 PyQt5 )
 for binding in "${array[@]}"
 do
-    docker run -ti --rm -v $(pwd):/Qt.py --entrypoint="python2.7" fredrikaverpil/qt.py:2017 /Qt.py/membership.py --binding=$binding
+    docker run -ti --rm -v $(pwd):/Qt.py --entrypoint="python2.7" fredrikaverpil/qt.py:2018 /Qt.py/membership.py --binding=$binding
 done
 
 # Rename QtGui key to QtWidgets
-docker run -ti --rm -v $(pwd):/Qt.py --entrypoint="python3.5" fredrikaverpil/qt.py:2017 /Qt.py/membership.py --copy-qtgui
+docker run -ti --rm -v $(pwd):/Qt.py --entrypoint="python3.5" fredrikaverpil/qt.py:2018 /Qt.py/membership.py --copy-qtgui
 
 # Generate common_members.json
-docker run -ti --rm -v $(pwd):/Qt.py --entrypoint="python3.5" fredrikaverpil/qt.py:2017 /Qt.py/membership.py --generate-common-members
+docker run -ti --rm -v $(pwd):/Qt.py --entrypoint="python3.5" fredrikaverpil/qt.py:2018 /Qt.py/membership.py --generate-common-members
 
 # Sort common members
-docker run -ti --rm -v $(pwd):/Qt.py --entrypoint="python3.5" fredrikaverpil/qt.py:2017 /Qt.py/membership.py --sort-common-members
+docker run -ti --rm -v $(pwd):/Qt.py --entrypoint="python3.5" fredrikaverpil/qt.py:2018 /Qt.py/membership.py --sort-common-members
