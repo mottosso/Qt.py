f475ff0896f210ec43c64c0389ede7d6e253ddfc
Mike <mike@hendricks3d.com>
2017-11-08 22:48:18 -0800

    Remove RuntimeError from _loadUi.

--- Qt.py
+++ Qt.py
@@ -1245,11 +1245,11 @@ def _loadUi(uifile, baseinstance=None):
         return the newly created instance of the user interface.
 
     """
-    if hasattr(baseinstance, "layout") and baseinstance.layout() \
-            and not baseinstance.layout().isEmpty():
-        message = ("QLayout: Attempting to add Layout to %s which "
-                   "already has a non-empty layout")
-        raise RuntimeError(message % (baseinstance))
+#    if hasattr(baseinstance, "layout") and baseinstance.layout() \
+#            and not baseinstance.layout().isEmpty():
+#        message = ("QLayout: Attempting to add Layout to %s which "
+#                   "already has a non-empty layout")
+#        raise RuntimeError(message % (baseinstance))
 
     if hasattr(Qt, "_uic"):
         return Qt._uic.loadUi(uifile, baseinstance)
