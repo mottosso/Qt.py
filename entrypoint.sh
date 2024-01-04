e4ffb10578a36fee35843dcbbb3f224d637da96e
Martin Chatterjee <martin@chatterjee.de>
2024-01-04 14:55:49 +0100

    entrypoint.sh: Switch to using pgrep to verify running Xvfb process.
    'pgrep Xvfb' identifies the process by name, which is a more robust test.
    
    The output of 'ps aux | grep Xvfb' inside the Docker container running on Apple silicon is slightly different
    and incompatible with the previous while condition:
    
    $ ps aux | grep Xvfb
    root      3025  0.0  0.4 1451928 32588 pts/0   Sl   14:25   0:00 /rosetta/rosetta /usr/bin/Xvfb Xvfb :99 -screen 0 1024x768x16
    root      3142  0.0  0.0 1206448 7808 pts/0    S+   14:32   0:00 /rosetta/rosetta /usr/bin/grep grep --color=auto Xvfb

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 
 # Start Xvfb
 Xvfb :99 -screen 0 1024x768x16 2>/dev/null &
-while ! ps aux | grep -q '[0]:00 Xvfb :99 -screen 0 1024x768x16'; do
+while ! pgrep 'Xvfb' &> /dev/null; do
     echo "Waiting for Xvfb..."
     sleep 1
 done
